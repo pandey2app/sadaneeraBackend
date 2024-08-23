7cdc09eb99a303c335afcef128c925387a7f1ff1
pandey2app <pandey2app@gmail.com>
2024-08-23 11:58:26 +0530

    changed user category

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,7 @@
-import { mongo } from "mongoose";
 import userModel from "../models/userModel.js";
 
 const getAllUsers = async (req, res)=>{
-    let {category : userCategory, name, isActive, sort, select, page, limit} = req.query;
+    let {userCategory, name, isActive, sort, select, page, limit} = req.query;
     let queryObject = {};
     if(userCategory){
         queryObject.userCategory = { $regex: userCategory, $options: 'i'};
@@ -40,4 +39,18 @@ const getAllUsersTesting = async (req, res)=>{
     res.status(200).json({users});
 }
 
-export {getAllUsers, getAllUsersTesting}
+//add user
+const addUser = async (req, res)=>{
+    const {name, email, password, role, userCategory, art} = req.body;
+    const newUser = await userModel.create({name, email, password, role, userCategory, art});
+    res.status(201).json({newUser});
+}
+
+//remove user
+const removeUser = async (req, res)=>{
+    const {_id} = req.user;
+    const deletedUser = await userModel.findByIdAndDelete(_id);
+    res.status(201).json({deletedUser});
+}
+
+export {getAllUsers, getAllUsersTesting, addUser, removeUser}
