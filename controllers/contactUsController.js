998872cceca43f82de5b92f1a6bbfe03a176be66
pandey2app <pandey2app@gmail.com>
2024-09-01 23:30:14 +0530

    removed logs

--- controllers/contactUsController.js
+++ controllers/contactUsController.js
@@ -3,11 +3,7 @@ import contactUsModel from "../models/contactUsModel.js";
 const addContactForm = async (req, res) => {
     const { name, email, mobile, subject, message } = req.body;
 
-    console.log(req.body);
-    
-    
     const newContactForm = await contactUsModel.create({ name, email, subject, mobile, message });
-    console.log(newContactForm);
     res.json(newContactForm);
 };
 
