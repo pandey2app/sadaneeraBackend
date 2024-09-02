5da03141d1a89321bdf861d08b91ad023af24014
pandey2app <pandey2app@gmail.com>
2024-09-02 15:47:37 +0530

    added delete feature in contact us data page

--- controllers/contactUsController.js
+++ controllers/contactUsController.js
@@ -8,8 +8,16 @@ const addContactForm = async (req, res) => {
 };
 
 const getAllContactForms = async (req, res) => {
+    
     const contactForms = await contactUsModel.find();
     res.json(contactForms);
 };
 
-export {addContactForm, getAllContactForms}
+const deleteContactForm = async (req, res) => {
+    const { id } = req.params;
+
+    const deletedContactForm = await contactUsModel.findOneAndDelete({ _id: id});
+    res.json(deletedContactForm);
+};
+
+export {addContactForm, getAllContactForms, deleteContactForm}
--- routes/contactUsRouter.js
+++ routes/contactUsRouter.js
@@ -1,10 +1,11 @@
 import express from'express'
-import { addContactForm, getAllContactForms } from '../controllers/contactUsController.js';
+import { addContactForm, deleteContactForm, getAllContactForms } from '../controllers/contactUsController.js';
 const router = express.Router();
 
 
 router.route('/').get(getAllContactForms);
 router.route('/add').post(addContactForm);
+router.route('/delete/:id').get(deleteContactForm);
 
 
 export default router;
