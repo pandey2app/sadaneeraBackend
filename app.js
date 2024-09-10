1e6c82613b500f9c523f087c5c0092d5630ff2a7
pandey2app <pandey2app@gmail.com>
2024-09-10 14:04:22 +0530

    added app file

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+
+// Routes
+import users_routes from'./routes/usersRouter.js'
+import user_routes from'./routes/userRouter.js'
+import contactUs_routes from'./routes/contactUsRouter.js'
+
+app.get('/', (req, res) => {
+    res.send('okay im live')
+})
+
+app.use('/api/users', users_routes)
+app.use('/api/user', user_routes)
+app.use('/api/contactUs', contactUs_routes)
+
+export default app;
