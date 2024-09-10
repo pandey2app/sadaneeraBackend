375f5098bea9be717ef390d2e65641328575acbe
pandey2app <pandey2app@gmail.com>
2024-09-10 14:38:43 +0530

    added static files directory

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 
@@ -9,9 +11,12 @@ app.use(cors({
     credentials: true,
 }));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static( __dirname + '/public'));
 app.use(cookieParser());
 
 // Routes
